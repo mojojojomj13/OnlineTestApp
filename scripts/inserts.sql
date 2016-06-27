038e3039b80743889d77d32cd7c225a1a3e786ed
mojo <mojo.jojo.mj13@gmail.com>
2016-06-27 09:29:13 +0530

    edited

--- scripts/inserts.sql
+++ scripts/inserts.sql
@@ -11,3 +11,8 @@ insert into users (username, password, enabled) values ('prit','Of2VibkzZuY1v0Ht
 --sample questions .
 INSERT INTO QUESTIONS (QUES_TEXT, QUES_OP1, QUES_OP2, QUES_OP3, QUES_OP4,  QUES_MRKS, CORRECT_OPS)  VALUES 
 ('Ques1', 'ops1', 'ops2', 'op3', 'ops4', 4, '{"ops1", "ops3"}'),('Ques2', 'ops21', 'ops22', 'op23', 'ops24', 3, '{"ops1"}') ;
+
+INSERT INTO USERS_DETAILS (USERNAME,FIRSTNAME,MIDDLENAME,LASTNAME,ADDRESS)
+VALUES
+('prit','Prithvish','Debashis','Mukherjee','Kolkata'),
+('student','Prithvish','Debashis','Mukherjee','Kolkata');
